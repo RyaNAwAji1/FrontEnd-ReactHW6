95acf8fe71c68f695da4839dbcaa2e545d3524bc
RyaN_AwAjI <102438965+RyaNAwAji1@users.noreply.github.com>
2022-06-21 11:17:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div  className="App" >
+      <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg'></img>
+      <p>First cat</p>
+      <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg'></img>
+      <p>Second Cat </p>
+      <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg'></img>
+      <p>Thired Cat</p>
+      <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg'></img>
+      <p>Fourth Cat</p>
+    
+    </div>
+  );
+}
+
+export default App;
